eb07ac5e7ef706e76a603c862b29dd6ed97095b7
Maxim Naydenko <mak.naydenko@gmail.com>
2020-04-26 11:04:21 +0200

    Fix File Opened For Write

--- probes/File Opened For Write/test.lua
+++ probes/File Opened For Write/test.lua
@@ -9,13 +9,10 @@ Packages {
 Case("mycase") {
   case = function()
     loadPackage("File Opened For Write")
-
     local fileName = fs.getTempDirectory() .. "\\testfile"
     local f = File.create(fileName, CREATE_ALWAYS, GENERIC_WRITE)
     f:close()
-
     unloadPackage("File Opened For Write")
-
     local events = fetchEvents("File Opened For Write")
     assert(#events ~= 0)
   end
