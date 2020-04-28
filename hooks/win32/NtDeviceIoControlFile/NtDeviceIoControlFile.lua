74a2ef8b368e393d3aabcabee44caf8f9110e46d
Anton Zhepko <anton.zhepko@hyperionix.com>
2020-04-28 17:05:31 +0300

    Add IOCTL Sended probe

---
+++ hooks/win32/NtDeviceIoControlFile/NtDeviceIoControlFile.lua
@@ -0,0 +1,18 @@
+Hook {
+  name = "NtDeviceIoControlFile",
+  target = "ntdll!NtDeviceIoControlFile",
+  decl = [[
+    __kernel_entry NTSTATUS NtDeviceIoControlFile(
+      IN HANDLE            FileHandle,
+      IN HANDLE            Event,
+      IN PIO_APC_ROUTINE   ApcRoutine,
+      IN PVOID             ApcContext,
+      OUT PIO_STATUS_BLOCK IoStatusBlock,
+      IN ULONG             IoControlCode,
+      IN PVOID             InputBuffer,
+      IN ULONG             InputBufferLength,
+      OUT PVOID            OutputBuffer,
+      IN ULONG             OutputBufferLength
+    );
+  ]]
+}
