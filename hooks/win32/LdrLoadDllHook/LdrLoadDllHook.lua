55ccab75fdcde37b0976509ea9bc0d347aea0c5b
Alexander Semakin <semakin.ai93@gmail.com>
2020-06-17 13:51:02 +0300

    Fix LdrLoadDll declaration (HOTFIX)

--- hooks/win32/LdrLoadDllHook/LdrLoadDllHook.lua
+++ hooks/win32/LdrLoadDllHook/LdrLoadDllHook.lua
@@ -3,8 +3,8 @@ Hook {
   target = "ntdll!LdrLoadDll",
   decl = [[
     NTSTATUS LdrLoadDll(
-      _In_opt_ PWCHAR      PathToFile,
       _In_opt_ ULONG       Flags,
+      _In_opt_ PULONG      Reserved,
       _In_ PUNICODE_STRING ModuleFileName,
       _Out_ PHANDLE        ModuleHandle);
   ]]
